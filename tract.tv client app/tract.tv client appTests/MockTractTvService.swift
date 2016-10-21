48cce82b247bca0f132472118670f67ef75f45d6
Sophie Clark <sophie.clark@marks-and-spencer.com>
2016-10-21 09:46:15 +0100

    remove mock session

---
+++ tract.tv client app/tract.tv client appTests/MockTractTvService.swift
@@ -0,0 +1,13 @@
+//
+//  MockTractTvService.swift
+//  tract.tv client app
+//
+//  Created by C2057175_Sophie_Clark on 21/10/2016.
+//  Copyright © 2016 Sophie Clark. All rights reserved.
+//
+
+import Foundation
+
+class MockTractTvService: TractTvService {
+  
+}
